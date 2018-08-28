6e1818e30519d89be8212b37ee2cf2b3de927e87
Organick <mcagriaksoy@ya.ru>
2018-08-28 17:09:00 +0300

    Bug fixes,
    change 'message' variable. Now it's value comes from text editor !

--- transmit.py
+++ transmit.py
@@ -2,14 +2,13 @@ __author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
 
 import socket
 import sys
+
 from PyQt5.QtWidgets import QApplication, QMenu, QMainWindow
 from PyQt5.uic import loadUi
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = ('localhost', 10000)
 
-message = b'This is the message.  It will be repeated.'
 
 
 class qt(QMainWindow):
@@ -17,7 +16,7 @@ class qt(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         loadUi('transmit.ui', self)
-        message = self.textEdit.toPlainText()
+
 
     def on_pushButton_clicked(self):
         self.completed = 0
@@ -27,7 +26,8 @@ class qt(QMainWindow):
 
             # Send data
         # print('sending {!r}'.format(message))
-        sock.sendto(message, server_address)
+        mesaj=self.textEdit.toPlainText().encode('utf-8')
+        sock.sendto(mesaj, server_address)
 
         # Receive response
         # print('waiting to receive')
